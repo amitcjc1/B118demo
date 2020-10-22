6328e0d943bd1a141ce7e4e87117d0589bebc616
amitcjc1 <61121166+amitcjc1@users.noreply.github.com>
2020-10-22 12:48:23 +0530

    Update Demo.java

--- GitDemo1/src/com/cjc/Demo.java
+++ GitDemo1/src/com/cjc/Demo.java
@@ -2,9 +2,18 @@ package com.cjc;
 
 public class Demo 
 {
+	public void m1()
+	{
+		System.out.println("M1===Git");
+	}
+	
+	
+	
 public static void main(String[] args) {
 	
 	System.out.println("Hello");
 }
+	
+	
 	
 }
